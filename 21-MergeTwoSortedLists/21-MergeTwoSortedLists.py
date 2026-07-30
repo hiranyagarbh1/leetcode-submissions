2129884fb29da0420613a1e9532379fd770caf8d
hiranyagarbh1 <69416129+hiranyagarbh1@users.noreply.github.com>
2026-07-29 23:12:05 -0400

    File updated at 29/07/2026, 23:12:05

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -1,14 +1,10 @@
-# Last updated: 29/07/2026, 23:03:17
+# Last updated: 29/07/2026, 23:12:05
 1class Solution:
-2    def removeElement(self, nums: List[int], val: int) -> int:
-3        k=0
-4        for element in range(len(nums)):
-5            if nums[element]!=val:
-6                nums[k]=nums[element]
-7                k+=1
-8        return k
-9
-10
-11
-12
-13        
+2    def removeDuplicates(self, nums: List[int]) -> int:
+3
+4        k = 1
+5        for i in range(1, len(nums)):
+6            if nums[i] != nums[k - 1]:
+7                nums[k] = nums[i]
+8                k += 1
+9        return k
